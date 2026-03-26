83fcae091f2040da75756251650007831b994d53
Jessica <jessicapasaribu22@gmail.com>
2026-03-26 12:13:59 +0700

    clean code

--- Case_Studi2/hospital-monolith/src/com/hospital/payment/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.payment;
-
-public class PaymentService {
-
-    public void processPayment(String user, int amount) {
-        System.out.println("Payment $" + amount + " processed for " + user);
-    }
-}
--- Case_Studi2/hospital-monolith/src/com/hospital/pharmacy/PharmacyService.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.pharmacy;
-
-public class PharmacyService {
-
-    public void orderMedicine(String user) {
-        System.out.println("Medicine ordered for " + user);
-    }
-}
